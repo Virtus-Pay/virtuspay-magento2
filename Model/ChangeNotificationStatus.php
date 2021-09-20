00f2895ecac5412123c280f33caaeeb0d5e85ba5
Ricardo Ferreira <ricardoconstantino30@gmail.com>
2021-09-20 15:04:58 -0300

    implementing webhook

---
+++ Model/ChangeNotificationStatus.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace VirtusPay\Magento2\Model;
+
+use VirtusPay\Magento2\Api\ChangeNotificationStatusInterface;
+use VirtusPay\Magento2\Helper\Data as HelperData;
+use Magento\Sales\Model\ResourceModel\Order\Payment\Transaction\CollectionFactory;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Psr\Log\LoggerInterface;
+
+class ChangeNotificationStatus implements ChangeNotificationStatusInterface
+{
+
+    const STATUS = [
+        'P' => 'pending_payment',
+        'N' => 'processing',
+        'A' => 'approved',
+        'R' => 'refused',
+        'C' => 'canceled',
+        'E' => 'complete'
+    ];
+
+    private $helperData;
+
+    private $collectionFactory;
+
+    private $orderRepositoryInterface;
+
+    private $logger;
+
+    public function __construct(
+        HelperData $helperData,
+        CollectionFactory $collectionFactory,
+        OrderRepositoryInterface $orderRepositoryInterface,
+        LoggerInterface $logger
+    ) {
+        $this->helperData = $helperData;
+        $this->collectionFactory = $collectionFactory;
+        $this->orderRepositoryInterface = $orderRepositoryInterface;
+        $this->logger = $logger;
+    }
+
+    public function execute(string $transactionId)
+    {
+
+        try {
+            $collection = $this->collectionFactory->create();
+            $result = $collection->getSelect()
+                ->where('txn_id = ?', $transactionId);
+            $transactionData = $result->query()->fetch();
+
+            $orderId = $transactionData['order_id'];
+            $data = $this->getOrderByTransactionSDK($transactionId);
+
+            $this->changeStatus($data['status'], $orderId);
+
+        } catch (\Exception $e) {
+             $this->logger->info("Error Status Webhook: " . $e);
+        }
+    }
+
+    private function getOrderByTransactionSDK(string $transactionId)
+    {
+        $response = "";
+        $configuration = new \VirtusPay\ApiSDK\Configuration();
+        $configuration->setEnvironment($this->helperData->getEnvironment());
+
+        $gateway = new \VirtusPay\ApiSDK\Gateway\Order();
+        $response = $gateway->getOrderByTransaction($transactionId);
+        $response = json_decode($response, true);
+
+        if (!empty($response)) {
+            return $response[0];
+        }
+
+        return $response;
+    }
+
+    private function changeStatus(string $status, string $orderId)
+    {
+        $order = $this->orderRepositoryInterface->get($orderId);
+        $order->setStatus(self::STATUS[$status]);
+        $order->setState(self::STATUS[$status]);
+
+        return $this->orderRepositoryInterface->save($order);
+    }
+
+}
